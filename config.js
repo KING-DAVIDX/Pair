78b5d432638c31490e4fab4b707bb121113a1f73
KING DAVID <xking6030@gmail.com>
2025-08-11 17:25:19 +0100

    Add config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  PREFIX: process.env.PREFIX || "ALYA-",
+  MESSAGE: process.env.MESSAGE || "Thank you for using our bot dont share your session id",
+  IMAGE: process.env.IMAGE || "https://files.catbox.moe/55f24l.jpg",
+  SUPABASE_URL: process.env.SUPABASE_URL || "https://cdvmjrpmrhvzwjutjqwc.supabase.co",
+  SUPABASE_KEY: process.env.SUPABASE_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNkdm1qcnBtcmh2endqdXRqcXdjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1Mzc4MjIzNywiZXhwIjoyMDY5MzU4MjM3fQ.XngWATkln_MgRDU8mog9DJjQ_wUwzy5GbyrRlSMULSc"
+}
